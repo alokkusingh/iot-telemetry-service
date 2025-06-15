4aa23b2b5164e15147e4bf9c4bcf8e3259ea9f0c
alokkusingh <alok.ku.singh@gmail.com>
2025-06-15 07:15:22 +0530

    first commit

---
+++ src/test/java/com/alok/iot/telemetry/TelemetryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alok.iot.telemetry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelemetryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
